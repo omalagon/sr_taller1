ece228cdad912251eaac64567a85a5219cfdd46e
Oscar Malagon <oscarmalagon@Oscars-MacBook-Air.local>
2020-03-11 21:26:51 -0500

    new model

--- login/models.py
+++ login/models.py
@@ -1,40 +1,34 @@
 from django.db import models
 from django.forms import ModelForm
 
+class Item(models.Model):
+    item = models.CharField(max_length=255, blank=True, null=True)
 
-# Models.
+    class Meta:
+        managed = False
+        db_table = 'item'
+
+
 class User(models.Model):
-    userid = models.CharField(max_length=100)
-    gender = models.CharField(max_length=100)
-    age = models.CharField(max_length=100)
-    country = models.CharField(max_length=100)
-    registered = models.DateTimeField(auto_now_add=True, blank=True)
+    user = models.CharField(max_length=255, blank=True, null=True)
+    password = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'user'
 
 
-class UserArtist(models.Model):
-    userid = models.CharField(max_length=100)
-    timestamp = models.CharField(max_length=100)
-    artid = models.CharField(max_length=100)
-    artname = models.CharField(max_length=100)
-    traid = models.CharField(max_length=100)
-    traname = models.CharField(max_length=100)
+class UserRating(models.Model):
+    user = models.CharField(max_length=255, blank=True, null=True)
+    item = models.CharField(max_length=255, blank=True, null=True)
+    rating = models.FloatField(blank=True, null=True)
 
-
-class Recommendations(models.Model):
-    userid = models.CharField(max_length=100)
-    recommendation = models.CharField(max_length=100)
-    rating = models.IntegerField()
-    recommendationType = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
+    class Meta:
+        managed = False
+        db_table = 'user_rating'
 
 # Forms
-class LoginForm(ModelForm):
-    class Meta:
-        model = User
-        fields = ['userid']
-
-
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ['userid', 'gender', 'age', 'country']
+        fields = ['user', 'password']
--- login/views.py
+++ login/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, HttpResponse, redirect
-from .models import User, Recommendations, UserForm, LoginForm
+from .models import User, UserForm
 
 
 def login(request):
-    form = LoginForm(request.POST or None)
+    form = UserForm(request.POST or None)
     if form.is_valid():
-        userid = form['userid'].data
-        result = User.objects.filter(userid=userid)
+        user = form['user'].data
+        password = form['password'].data
+
+        result = User.objects.filter(user=user, password=password)
         if result and len(result) == 1:
-            request.session['userid'] = userid
+            request.session['user'] = user
             return redirect('recommendations')
         else:
             return redirect('register')
@@ -20,38 +22,37 @@ def register(request):
     form = UserForm(request.POST or None)
     if form.is_valid():
         user = User()
-        user.userid = form['userid'].data
-        user.gender = form['gender'].data
-        user.age = form['age'].data
-        user.country = form['country'].data
+        user.user = form['user'].data
+        user.password = form['password']
 
         User.save(user)
-        request.session['userid'] = user.userid
+        request.session['user'] = user.user
         return redirect('recommendations')
     else:
-        return render(request, "login/create.html", context={'form': form})
+        return redirect('register')
 
 def recommendations(request):
-    userid = request.session['userid']
-    print(userid)
-    recommendations_result = Recommendations.objects.filter(userid=userid).order_by('-rating')
-
-    jaccard_u = recommendations_result.filter(recommendationType='jaccard').filter(model='u')
-    cosine_u = recommendations_result.filter(recommendationType='cosine').filter(model='u')
-    pearson_u = recommendations_result.filter(recommendationType='pearson').filter(model='u')
-
-    jaccard_t = recommendations_result.filter(recommendationType='jaccard').filter(model='t')
-    cosine_t = recommendations_result.filter(recommendationType='cosine').filter(model='t')
-    pearson_t = recommendations_result.filter(recommendationType='pearson').filter(model='t')
-
-    return render(request, "login/recommendations.html", context=
-        {
-            'userid': userid,
-            'jaccard_u': jaccard_u,
-            'cosine_u': cosine_u,
-            'pearson_u': pearson_u,
-            'jaccard_t': jaccard_t,
-            'cosine_t': cosine_t,
-            'pearson_t': pearson_t,
-         }
-                  )
+    return render(request, 'login/recommendations.html')
+    # user = request.session['user']
+    # print(user)
+    # recommendations_result = Recommendations.objects.filter(user=user).order_by('-rating')
+    #
+    # jaccard_u = recommendations_result.filter(recommendationType='jaccard').filter(model='u')
+    # cosine_u = recommendations_result.filter(recommendationType='cosine').filter(model='u')
+    # pearson_u = recommendations_result.filter(recommendationType='pearson').filter(model='u')
+    #
+    # jaccard_t = recommendations_result.filter(recommendationType='jaccard').filter(model='t')
+    # cosine_t = recommendations_result.filter(recommendationType='cosine').filter(model='t')
+    # pearson_t = recommendations_result.filter(recommendationType='pearson').filter(model='t')
+    #
+    # return render(request, "login/recommendations.html", context=
+    #     {
+    #         'user': user,
+    #         'jaccard_u': jaccard_u,
+    #         'cosine_u': cosine_u,
+    #         'pearson_u': pearson_u,
+    #         'jaccard_t': jaccard_t,
+    #         'cosine_t': cosine_t,
+    #         'pearson_t': pearson_t,
+    #      }
+    #               )
