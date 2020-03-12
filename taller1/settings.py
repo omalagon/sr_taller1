6639ab8b300ab4e71c059b04795c1b604bbac0ea
Oscar Malagon <oscarmalagon@Oscars-MacBook-Air.local>
2020-03-11 21:08:40 -0500

    mysql integration

--- taller1/settings.py
+++ taller1/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'taller1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sr',
+        'USER': 'sr',
+        'PASSWORD': 'grupo09.grupo09',
+        'HOST': '172.24.100.60',
+        'PORT': '3306',
     }
 }
 
