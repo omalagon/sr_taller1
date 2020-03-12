d8ff3f84a69cf2fc56c6e6cee6e283191ded0c6b
Oscar Malagon <oscarmalagon@Oscars-MacBook-Air.local>
2020-03-11 21:31:51 -0500

    new model

--- login/views.py
+++ login/views.py
@@ -13,7 +13,7 @@ def login(request):
             request.session['user'] = user
             return redirect('recommendations')
         else:
-            return render(request, 'login/create.html')
+            return render(request, 'login/create.html', context={'form': form})
     else:
         return render(request, "login/login.html", context={'form': form})
 
@@ -29,7 +29,7 @@ def register(request):
         request.session['user'] = user.user
         return redirect('recommendations')
     else:
-        return render(request, 'login/create.html')
+        return render(request, 'login/create.html', context={'form': form})
 
 
 def recommendations(request):
